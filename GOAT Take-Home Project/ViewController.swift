68deb2e9130f09dbceac590431494cc0a70d4c11
Valerie Don <valeriejdon@gmail.com>
2020-01-25 11:10:59 -0800

    Initial Commit

---
+++ GOAT Take-Home Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GOAT Take-Home Project
+//
+//  Created by Valerie Don on 1/25/20.
+//  Copyright © 2020 Valerie Don. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
