fac1721a975e12ded7a9d75e8612378eb7653345
Valerie Don <valeriejdon@gmail.com>
2020-01-26 02:08:01 -0800

    Make tableview automatically adjust height, clean up code in forecasttableviewcell

--- GOAT Take-Home Project/ForecastTableViewCell.swift
+++ GOAT Take-Home Project/ForecastTableViewCell.swift
@@ -13,15 +13,4 @@ class ForecastTableViewCell: UITableViewCell {
     @IBOutlet weak var iconImageView: UIImageView!
     @IBOutlet weak var forecastLabel: UILabel!
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
 }
